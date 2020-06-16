7f2a75f837900564eda8f811d2b7fac1fd6ca963
Zhonghong Huang <zhohuang@microsoft.com>
2020-06-16 15:33:51 +0800

    A script install and integrate vcpkg

---
+++ dev_setup.ps1
@@ -0,0 +1,9 @@
+# Invoke-WebRequest -Uri "https://github.com/microsoft/vcpkg/archive/2020.04.zip" -OutFile "vcpkg.zip"
+# Expand-Archive -Path vcpkg.zip
+Set-Location vcpkg\vcpkg-2020.04
+# ./bootstrap-vcpkg.bat
+$IntegrateResult = ./vcpkg integrate project
+$InstallCmd = ($IntegrateResult -split "\n")[-2].Trim()
+Write-Output $InstallCmd
+
+Set-Location ../..
